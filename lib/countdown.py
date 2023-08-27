b2e13455007a728410ea28d010b9ee3417e30f99
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-08-27 09:50:34 -0500

    countdown

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,21 @@
-# your code goes here!
+import time
+def countdown(num):
+    i=0
+    while i<=num:
+        if(i<num):
+            print(f'{num-i} SECOND(S)!')
+            i+=1
+        elif(i==num):
+            print('HAPPY NEW YEAR!')
+            i+=1
+
+def countdown_with_sleep(num):
+    i=0
+    while i<=num:
+        time.sleep(1)
+        if(i<num):
+            print(f'{num-i} SECOND(S)!')
+            i+=1
+        elif(i==num):
+            print('HAPPY NEW YEAR!')
+            i+=1
